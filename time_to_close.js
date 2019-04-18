b20b1ff51ea8e14fdafb6f64fa56ee23e25f2e89
RafidaZaman <35875281+RafidaZaman@users.noreply.github.com>
2019-04-18 16:21:21 -0500

    Update time_to_close.js

--- time_to_close.js
+++ time_to_close.js
@@ -35,7 +35,12 @@ app.controller("MinCtrl",function ($scope,$http) {
                                     $http.get("https://data.kcmo.org/resource/cyqf-nban.json?$select=avg(days_to_close)&creation_year=2017").then(function (value) {
 
                                         $scope.avg_days_2017 = Math.round(value.data[0].avg_days_to_close);
-                                        drawChartx($scope.avg_days_2010, $scope.avg_days_2011, $scope.avg_days_2012, $scope.avg_days_2013, $scope.avg_days_2014, $scope.avg_days_2015, $scope.avg_days_2016, $scope.avg_days_2017, 'kc_chart1');
+                                        
+                                        $http.get("https://data.kcmo.org/resource/cyqf-nban.json?$select=avg(days_to_close)&creation_year=2018").then(function (value) {
+
+                                           $scope.avg_days_2018 = Math.round(value.data[0].avg_days_to_close);
+                                            drawChartx($scope.avg_days_2010, $scope.avg_days_2011, $scope.avg_days_2012, $scope.avg_days_2013, $scope.avg_days_2014, $scope.avg_days_2015, $scope.avg_days_2016, $scope.avg_days_2017, $scope.avg_days_2018, 'kc_chart1');
+                                        });
                                     });
                                 });
                             });
@@ -67,8 +72,9 @@ app.controller("MinCtrl",function ($scope,$http) {
         $scope.avg_days_2014 = 29;
         $scope.avg_days_2015 = 19;
         $scope.avg_days_2016 = 16;
-        $scope.avg_days_2017 = 10;
-        drawChartx($scope.avg_days_2010, $scope.avg_days_2011, $scope.avg_days_2012, $scope.avg_days_2013, $scope.avg_days_2014, $scope.avg_days_2015, $scope.avg_days_2016, $scope.avg_days_2017, 'bs_chart1');
+        $scope.avg_days_2017 = 18;
+        $scope.avg_days_2018 = 12;
+        drawChartx($scope.avg_days_2010, $scope.avg_days_2011, $scope.avg_days_2012, $scope.avg_days_2013, $scope.avg_days_2014, $scope.avg_days_2015, $scope.avg_days_2016, $scope.avg_days_2017, $scope.avg_days_2018, 'bs_chart1');
     };
 
     $scope.dvfunction = function () {
@@ -105,7 +111,7 @@ app.controller("MinCtrl",function ($scope,$http) {
 
     google.charts.load('current', {'packages': ['corechart']});
     google.charts.setOnLoadCallback(drawChartx);
-    function drawChartx(var0, var1, var2, var3, var4, var5, var6, var7, var8) {
+    function drawChartx(var0, var1, var2, var3, var4, var5, var6, var7, var8, var9) {
         console.log(var0);
         var data = google.visualization.arrayToDataTable([
             ['Year','Number of Days Taken(AVG)'],
@@ -116,7 +122,8 @@ app.controller("MinCtrl",function ($scope,$http) {
             ['2014', parseInt(var4)],
             ['2015', parseInt(var5)],
             ['2016', parseInt(var6)],
-            ['2017', parseInt(var7)]
+            ['2017', parseInt(var7)],
+            ['2018', parseInt(var8)]
         ]);
         var options = {
             title: 'Time_to_close ',
@@ -127,7 +134,7 @@ app.controller("MinCtrl",function ($scope,$http) {
             series: {0: { pointShape: 'circle' }},
             colors : ['#e7711b']
         };
-        var chart = new google.visualization.LineChart(document.getElementById(var8));
+        var chart = new google.visualization.LineChart(document.getElementById(var9));
         chart.draw(data, options);
     }
 });
