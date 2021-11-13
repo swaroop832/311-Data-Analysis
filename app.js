33f9c7f146c6a62e7cc481a8faaa74b383d76ac0
RafidaZaman <35875281+RafidaZaman@users.noreply.github.com>
2021-11-12 23:52:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -940,7 +940,7 @@ app.controller("MinCtrl",function ($scope,$http) {
         {
             "Year" : 2020,
             "Kansas_City" : 508090,
-            "NewYork_City": 8804000,
+            "NewYork_City": 8804190,
             "Austin": 947890,
             "Baton_Rouge": 227715,
             "Boston" :673184,
